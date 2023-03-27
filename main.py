1ea9a984b0e856b21e63846e51d44105732476ef
Guillaume Venet - pc_foed <venetguillaume@gmail.com>
2023-03-27 12:32:03 +0200

    db init

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 import sqlite3
 from flask import g
+from pathlib import Path
 
 app = Flask(__name__)
 
-DATABASE = '/path/to/database.db'
+DATABASE = 'database.db'
+SQL_SCRIPT = 'db/database.sql'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -18,13 +20,26 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
-        
-
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/login")
+def index():
+    # cur = get_db().cursor()
+    return render_template('login.html')
 
 @app.route('/home')
 def home():
     return render_template('index.html')
 
+def main():
+    if not Path(DATABASE).exists():
+        print('create db')
+        with open(SQL_SCRIPT, 'r') as sql_file:
+            sql_script = sql_file.read()
+
+        db = sqlite3.connect(DATABASE)
+        cursor = db.cursor()
+        cursor.executescript(sql_script)
+        db.commit()
+        db.close()
+
+main()
