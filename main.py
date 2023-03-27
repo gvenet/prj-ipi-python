6b2a3a5182139daa27a3fe196fc79d3486a5f8f8
Your Name <you@example.com>
2023-03-27 10:28:47 +0200

    ajout sql + homepage

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
+from flask import g
 
 app = Flask(__name__)
 
+DATABASE = '/path/to/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+        
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
